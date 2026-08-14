e5d056cec5c3e7d0f49fa97283344ce474861e1e
Georvin <georfrankvinces0616@gmail.com>
2026-08-15 00:35:28 +0200

    Update downloaderapp.py

--- downloaderapp.py
+++ downloaderapp.py
@@ -39,8 +39,8 @@ def descargar():
             'cookiefile': os.path.join(BASE_DIR, 'cookies.txt'),
             'extractor_args': {
                 'youtube': {
-                  'player_client': ['ios', 'android', 'web_creator'],
-                    'player_skip': ['webpage', 'configs']
+                  'player_client': ['mweb', 'ios'],
+                    
                 }
             },
     } 
